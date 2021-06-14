b04999ea1e1cdfb475a2f6483a4af9f81f611f55
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-06-14 18:41:52 +0800

    cleaned repos

---
+++ 05_chainlink_moralis/contracts/price_feed.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+import "alphachainio/chainlink-contracts@1.1.2/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract Price_ETH{
+
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getThePrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
---
+++ 05_chainlink_moralis/scripts/network_settings.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+brownie networks add Ethereum rinkeby_test host='https://speedy-nodes-nyc.moralis.io/f9c80ab8c7ec06447269db5c/eth/rinkeby' chainid=4 explorer='https://api-rinkeby.etherscan.io/api'
