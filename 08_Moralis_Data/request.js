9b51948381563fa1b53d681d62387c36f3f7c9fb
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-06-30 12:05:27 +0800

    ADDING MORALIS DATA

--- 08_Moralis_Data/request.js
+++
@@ -1,8 +0,0 @@
-const response = await fetch('YOURLINK', {
-    method: 'GET',
-    headers: new Headers({
-        'accept':'*/*',
-        'Authorization': 'YOUR AUTHORIZATION JWT'
-    })
-})
-console.log(await response.json())
