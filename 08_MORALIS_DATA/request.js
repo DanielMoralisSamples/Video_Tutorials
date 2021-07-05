4adb89fd19db976968f63d4618862951f6b987c3
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-07-05 09:44:42 +0800

    added api response

--- 08_MORALIS_DATA/request.js
+++ 08_MORALIS_DATA/request.js
@@ -1,8 +1,8 @@
-const response = await fetch('YOURLINK', {
+const response = await fetch('https://to6yj724p7wc.moralis.io:2053/api/account/erc20/balances?chain=bsc&chain_name=testnet&address=0xbA6AADA3F43521296f92cdD6730108E13b927c1c', {
     method: 'GET',
     headers: new Headers({
         'accept':'*/*',
-        'Authorization': 'YOUR AUTHORIZATION JWT'
+        'Authorization':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoaXN0b3JpY0FwaSI6dHJ1ZSwiYWNjb3VudEFwaSI6dHJ1ZSwidG9rZW5BcGkiOnRydWUsImlhdCI6MTYyNTAzNTA0MCwiZXhwIjoxNjI1MDM4NjQwfQ.nRPGLWr4SB-V1IfENqoAW91Aeo7wZaWYTc5N3PXAYD8'
     })
 })
 console.log(await response.json())
