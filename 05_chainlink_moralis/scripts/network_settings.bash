084a7eca36790bdf6d155d6c797dbb19c5796325
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-07-13 17:32:53 +0800

    current to july 13

--- 05_chainlink_moralis/scripts/network_settings.bash
+++ 05_chainlink_moralis/scripts/network_settings.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 brownie networks add Ethereum rinkeby_test host='YOUR LINK' name="Eth Rinkeby Moralis" chainid=4 explorer='https://api-rinkeby.etherscan.io/api'
 brownie networks add "Binance Smart Chain" bsc_test_moralis host='YOUR LINK' name="Testnet (Moralis)" chainid=97 explorer='https://api-testnet.bscscan.com/api'
-brownie networks add Polygon mumbai_moralis host='YOUR LINK'name='Mumbai (Moralis)' chainid=80001 explorer='https://explorer-mumbai.maticvigil.com/'
+brownie networks add Polygon mumbai_moralis host='YOUR LINK' name='Mumbai (Moralis)' chainid=80001 explorer='https://explorer-mumbai.maticvigil.com/'
