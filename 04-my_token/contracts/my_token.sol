f690ea15f2841c9e85916bceda0b87808443048c
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-07-20 15:28:07 +0800

    current to July 20 2021

--- 04-my_token/contracts/my_token.sol
+++ 04-my_token/contracts/my_token.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC20/ERC20.sol";
 
 
-contract DanielToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Daniel", "Dan") {
+contract TheFellowShip is ERC20 {
+    constructor(uint256 initialSupply) ERC20("LordOFTheRings", "Tolkien") {
         _mint(msg.sender, initialSupply);
     }
 }
--- 10_Flask_Dapp/views.py
+++
@@ -1,8 +0,0 @@
-import app
-from flask import render_template
-from flask import request, redirect, jsonify, make_response
-import json
-
-@app.route('/')
-def index():
-    return render_template("index.html")
